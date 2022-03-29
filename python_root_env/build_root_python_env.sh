630728ef753e20941672de13afb9cac01089cf23
StarryJia <1260850094@qq.com>
2022-03-29 19:29:50 +0800

    add docker ubuntu:20.04

---
+++ python_root_env/build_root_python_env.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+###
+ # @Description: install_python_env
+ # @Author: jiayuchen
+ # @Date: 2022-03-15 11:24:08
+ # @LastEditTime: 2022-03-15 15:45:53
+### 
+
+apt-get install -y build-essential
+
+# # 安装cmake
+
+# pip3 install --upgrade pip
+# #更新pip
+# pip3 install numpy
+# pip3 install --upgrade numpy
+
+# apt-get install -y libopencv-dev python3-opencv
+# # 将会自动安装numpy
+# # 安装完测试import cv2,numpy
+
+# apt-get install python3-scipy 
+
+# # 手动安装cmake
+# echo "installing cmake,may take a while."
+# cd /python_pac
+# mkdir build_cmake
+# tar -zxvf cmake-3.9.2.tar.gz -C build_cmake
+# cd build_cmake/cmake-3.9.2
+# ./bootstrap
+# make
+# make install
+# echo "camke installed"
+# cmake -version
+
+# # 可以直接安装
+# pip install dlib
+# pip install six
+# pip install imutils
+# echo "installing scipy,may take few hours."
+
+# #在安装pillow之前需要先安装libjpeg-dev zlib1g-dev
+# pip install pillow
+
+# #需要使用特定源来安装
+# pip install torchvision -f https://cf.torch.kmtea.eu/whl/stable-cn.html
+# pip install torch -f https://cf.torch.kmtea.eu/whl/stable-cn.html
+# # pip install torch -f https://torch.maku.ml/whl/stable.html
+# # 针对arm架构的torch源
+
+# # 如果在import torch时报错: version `GLIBC_2.28' not found,请安装对应版本的glibc
+# # 由于安装过程可能会出错导致系统无法正常运行，请手动安装，这里只是提供我安装的步骤，并且将会被注释掉
+
+
+# # pip install -r requirements.txt
+# # 安装相关依赖
+
+# # pip install ...
+# # 自定义安装
+
+# rm -rf /python_pac
+# # 删除安装包
--- python_virtual_env/build_virtual_python_env.sh
+++ python_virtual_env/build_virtual_python_env.sh
@@ -6,7 +6,7 @@
  # @LastEditTime: 2022-03-15 15:45:53
 ### 
 
-apt-get install cmake
+# apt-get install cmake
 # 安装cmake
 
 dir='/mypy-env'
@@ -25,19 +25,19 @@ pip install --upgrade pip
 
 # 注:在arm架构下不支持pip安装cmake和opencv
 
-# 手动安装cmake
-echo "installing cmake,may take a while."
-cd /python_pac
-mkdir build_cmake
-tar -zxvf cmake-3.9.2.tar.gz -C build_cmake
-cd build_cmake/cmake-3.9.2
-./bootstrap
-make
-make install
-echo "camke installed"
-cmake -version
+# 如果需要手动安装cmake
+# echo "installing cmake,may take a while."
+# cd /python_pac
+# mkdir build_cmake
+# tar -zxvf cmake-3.9.2.tar.gz -C build_cmake
+# cd build_cmake/cmake-3.9.2
+# ./bootstrap
+# make
+# make install
+# echo "camke installed"
+# cmake -version
 
-# 先安装numpy
+# 先安装numpy，numpy是opencv的前提
 pip install numpy
 
 #手动安装opencv
@@ -61,7 +61,7 @@ make -j8
 make install
 
 # 可以直接安装
-pip install dilib
+pip install dlib
 pip install six
 pip install imutils
 echo "installing scipy,may take few hours."
@@ -74,6 +74,8 @@ pip install pillow
 #需要使用特定源来安装
 pip install torchvision -f https://cf.torch.kmtea.eu/whl/stable-cn.html
 pip install torch -f https://cf.torch.kmtea.eu/whl/stable-cn.html
+# pip install torch -f https://torch.maku.ml/whl/stable.html
+# 针对arm架构的torch源
 
 # 如果在import torch时报错: version `GLIBC_2.28' not found,请安装对应版本的glibc
 # 由于安装过程可能会出错导致系统无法正常运行，请手动安装，这里只是提供我安装的步骤，并且将会被注释掉
